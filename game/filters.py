e47b9d30783869d66841b939c72ba53d2d0b44e2
iwilusz <izaw0508@wp.pl>
2022-05-29 19:13:39 +0200

    website design vol2

--- game/filters.py
+++ game/filters.py
@@ -1,7 +1,25 @@
 import django_filters
 from .models import Game
+from crispy_forms.helper import FormHelper
+from django import forms
+
 
 class GameFilter(django_filters.FilterSet):
+
+
     class Meta:
         model = Game
         fields = ['player', 'good_answers', 'bad_answers', 'state','result']
+
+    def __init__(self, *args, **kwargs):
+        super(GameFilter, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        # self.helper.layout = Layout(
+        #     Fieldset(
+        #         'Player',
+        #         'Good answers',
+        #         'Bad answers',
+        #         'State',
+        #         'Result', wrapper_class="extra-class"
+        #     ))
+
